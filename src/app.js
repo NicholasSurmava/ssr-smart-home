676e2d79895ef2c4f55913ef7dbd6f612eb6a552
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-24 17:53:30 -0600

    created simple getWeather function, http get

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const https = require("https");
 
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "ejs");
@@ -9,6 +10,28 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/
 // TODO: get request for weather ^
+
+function getWeather() {
+  let weather_url =
+    "https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/";
+  https
+    .get(weather_url, (response) => {
+      let weather = "";
+
+      // called when a data chunk is received.
+      response.on("data", (chunk) => {
+        weather += chunk;
+      });
+
+      // called when the complete response is received.
+      response.on("end", () => {
+        console.log(`Weather data fetched from: ${weather_url}`);
+      });
+    })
+    .on("error", (error) => {
+      console.log("Error: " + error.message);
+    });
+}
 
 let powerInfo = {
   batteries: "good",
@@ -28,7 +51,10 @@ let internetInfo = {
 };
 
 app.get("/", (req, res) => {
+  let weather = getWeather();
+
   const viewData = {
+    weather,
     powerInfo,
     waterInfo,
     internetInfo,
