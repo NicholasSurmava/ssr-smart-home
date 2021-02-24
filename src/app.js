e25280fb463b41a3708b2dba866eb82573efc8c3
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-24 17:29:51 -0600

    created index html file and set it up in express file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.json({
-    heartbeat: "badum badum badum",
-  });
+  res.render("index");
 });
 
 module.exports = app;
