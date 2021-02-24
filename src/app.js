5aff3a5d21676697458a0dfaa23a621ef9f6fc3f
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-24 17:44:43 -0600

    setup some data for home report

--- src/app.js
+++ src/app.js
@@ -7,8 +7,34 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/
+// TODO: get request for weather ^
+
+let powerInfo = {
+  batteries: "good",
+  breakers: "good",
+  voltage: "26.7v",
+};
+
+let waterInfo = {
+  water_level: "good",
+  water_quality: "good",
+};
+
+let internetInfo = {
+  isp: "Comcast",
+  speed: "1000 mbps",
+  internet_health: "good",
+};
+
 app.get("/", (req, res) => {
-  res.render("index");
+  const viewData = {
+    powerInfo,
+    waterInfo,
+    internetInfo,
+  };
+
+  res.render("index", viewData);
 });
 
 module.exports = app;
