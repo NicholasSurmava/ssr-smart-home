d520037437512e75b07d1e0d9ef759ba594cbedd
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-24 19:37:37 -0600

    replaced https module request with node-fetch

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const https = require("https");
+const fetch = require("node-fetch");
 
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "ejs");
@@ -10,28 +10,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/
 // TODO: get request for weather ^
-
-function getWeather() {
-  let weather_url =
-    "https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/";
-  https
-    .get(weather_url, (response) => {
-      let weather = "";
-
-      // called when a data chunk is received.
-      response.on("data", (chunk) => {
-        weather += chunk;
-      });
-
-      // called when the complete response is received.
-      response.on("end", () => {
-        console.log(`Weather data fetched from: ${weather_url}`);
-      });
-    })
-    .on("error", (error) => {
-      console.log("Error: " + error.message);
-    });
-}
+getWeather = async () => {
+  try {
+    const response = await fetch(
+      "https://api.oceandrivers.com:443/v1.0/getAemetStation/aeropuertopalma/lastdata/"
+    );
+    const weather = await response.json();
+    return weather;
+  } catch (err) {
+    // handle error
+    console.error(err);
+  }
+};
 
 let powerInfo = {
   batteries: "good",
@@ -51,16 +41,17 @@ let internetInfo = {
 };
 
 app.get("/", (req, res) => {
-  let weather = getWeather();
-
   const viewData = {
-    weather,
     powerInfo,
     waterInfo,
     internetInfo,
   };
 
-  res.render("index", viewData);
+  (async () => {
+    const weatherInfo = await getWeather();
+    viewData["weatherInfo"] = weatherInfo;
+    res.render("index", viewData);
+  })();
 });
 
 module.exports = app;
